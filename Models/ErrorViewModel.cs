99593a0e2965a382c9f9b9520836ca8852ead739
Felix Gramell <felixg.nti@gmail.com>
2020-02-13 11:47:51 +0100

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tranbär.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
